589437b2b3fa0261afff95670a288a0e22202d19
Erdem Kapkara <erdemkapkara.uk@gmail.com>
2022-02-21 16:13:45 +0000

    FirstTemplate

---
+++ Functions2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        static int maxTrailing(int arr_count, int[] arr)
+        {
+
+            int maxDifference = int.MinValue;
+
+            for (int i = 0; i < arr_count; i++)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (maxDifference < arr[i] - arr[j])
+                    {
+                        maxDifference = arr[i] - arr[j];
+                    }
+                }
+            }
+
+            if (maxDifference == int.MinValue)
+            {
+                return -1;
+            }
+            else
+            {
+                return maxDifference;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int count = 10;
+            int[] array = { 1, 2, 3, 4, 15,6,7,8,9,10 };
+            
+            Console.WriteLine(Program.maxTrailing(count, array));
+        }
+
+       
+    }
+}
